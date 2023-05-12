b62290fe588acc384fb1c94de18e01938003b67f
Rohan Khanna <rohankhanna190@gmail.com>
2023-05-12 10:45:45 +0530

    OK

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -82,7 +82,6 @@ function App({ user, type }) {
     const res = await fetch(`${process.env.REACT_APP_API_PROXY}/logout`, {
       method: "GET",
       headers: {
-        mode:'no-cors',
         "Content-Type": "application/json",
         "Authorization": token,
         Accept: "application/json"
