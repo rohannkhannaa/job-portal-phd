00ef79e7f635f41f480b7aed9194c35b4fd5620e
Rohan Khanna <rohankhanna190@gmail.com>
2023-05-15 00:16:41 +0530

    response-check

--- client/src/components/Academic.js
+++ client/src/components/Academic.js
@@ -242,7 +242,7 @@ export default function Profile({ user, type }) {
           <>
             {isedit1 ? (
               <>
-                <div style={{ width: "90%" }} className="flex my-10">
+                <div style={{ width: isMobile ? "100%":"90%" }} className="flex my-10">
                   <div className="my-2 flex-1 bg-white shadow overflow-hidden sm:rounded-lg">
                     <div className="flex space-x-3 px-4 py-5 sm:px-6">
                       <h3 className="text-lg leading-6 font-medium text-gray-900">
@@ -381,7 +381,7 @@ export default function Profile({ user, type }) {
               </>
             ) : (
               <>
-                <div style={{ width: "90%" }} className="flex my-10 mx-20">
+                <div style={{ width: isMobile ? "100%" :  "90%" }} className={isMobile ? "flex my-10": "flex my-10 mx-20"}>
                   <div className="my-2 flex-1 bg-white shadow overflow-hidden sm:rounded-lg">
                     <div className="flex space-x-3 px-4 py-5 sm:px-6">
                       <h3 className="text-lg leading-6 font-medium text-gray-900">
