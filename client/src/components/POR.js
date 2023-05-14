4670367511581549e90a2769e83ab7beac9211e9
Rohan Khanna <rohankhanna190@gmail.com>
2023-05-15 01:33:47 +0530

    OK now

--- client/src/components/POR.js
+++ client/src/components/POR.js
@@ -369,7 +369,7 @@ const POR = ({ user, type }) => {
                           </dt>
                           <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
                             <input
-                              type="text"
+                              type="text" 
                               name="title"
                               value={formData.title}
                               onChange={handleChange}
@@ -471,7 +471,7 @@ const POR = ({ user, type }) => {
           ) : (
             <>
               <div className="userProfile">
-                <div style={{ width: "90%" , marginRight : '0rem'}} className="flex my-10 mx-20">
+                <div style={{ width: "90%" , marginLeft : '0rem'}} className="flex my-10 mx-20">
                   <div className="my-2 flex-1 bg-white shadow overflow-hidden sm:rounded-lg">
                     <div className="flex space-x-3 px-4 py-5 sm:px-6">
                       <h3 className="text-lg leading-6 font-medium text-gray-900">
