fb884fa32cb6f9556228e88954a63c19b88695d3
Rohan Khanna <rohankhanna190@gmail.com>
2023-05-14 23:51:32 +0530

    responsive okok

--- client/src/components/OtherDetails.js
+++ client/src/components/OtherDetails.js
@@ -82,7 +82,7 @@ function FileUpload({ user, type }) {
         </div>)}
       </div>
       <hr />
-      <div style={{ display: "flex" }}>
+     {!isMobile?  (<div style={{ display: "flex" }}>
         <div>
           <form onSubmit={handleSubmit} className="userProfile">
             <div
@@ -244,7 +244,170 @@ function FileUpload({ user, type }) {
             </div>
           </>
         )}
+      </div>):(<>
+        <div style={{ display: "flex", flexDirection : 'column' }}>
+        <div>
+          <form onSubmit={handleSubmit} className="userProfile">
+            <div
+              style={{ padding: "0" }}
+              className="col-span-full sm:col-span-full"
+            >
+              <label
+                className="block mb-2 text-sm font-medium text-gray-700 dark:text-gray-300"
+                htmlFor="resume"
+              >
+                Upload your recent resume
+                <span style={{ color: "#ff0000" }}> *</span>
+                {resume ? (
+                  <>
+                    <div
+                      style={{
+                        width: "fit-content",
+                        position: "relative",
+                        margin: "0",
+                        // padding: "2rem",
+                        border: "1px solid grey",
+                      }}
+                    >
+                      <Document
+                        style={{
+                          margin: "0",
+                          padding: "0",
+                        }}
+                        file={resume}
+                        onLoadSuccess={onDocumentLoadSuccess}
+                        noData
+                        options={{ disableAutoFetch: true }}
+                      >
+                        <Page
+                          style={{
+                            margin: "0",
+                            padding: "0",
+                          }}
+                          pageNumber={1}
+                          width={200}
+                          height={300}
+                          scale={0.5}
+                          renderTextLayer={false}
+                          renderAnnotationLayer={false}
+                        />
+                      </Document>
+                    </div>
+                  </>
+                ) : (
+                  <></>
+                )}
+              </label>
+
+              <input
+                className="block w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 cursor-pointer dark:text-gray-400 focus:outline-none focus:border-transparent dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
+                aria-describedby="profile-picture-desc"
+                id="resume"
+                type="file"
+                required
+                accept=".docs, .doc, .pdf, .docx"
+                onChange={convertToBase64}
+              />
+              <div
+                className="mt-1 text-sm text-gray-500 dark:text-gray-300"
+                id="profile-picture-desc"
+              >
+                <br />
+                <span className="font-semibold">Maximum file size:</span> 20 MB,{" "}
+                <span className="font-semibold">Allowed file formats:</span>{" "}
+                .pdf, .docs, .docx
+                <br />
+                <br />
+                <div className="mt-1">
+                  <span className="font-semibold">
+                    Recommended File Name Format:
+                  </span>
+                  <span>
+                    {" "}
+                    Your_name_&lt;resume&gt; <br />
+                    Example: Rohan_resume
+                  </span>
+                </div>
+              </div>
+            </div>
+            <button
+              style={{
+                color: "#3b32b3",
+                border: "1px solid #3b32b3",
+                padding: "0.3rem",
+                borderRadius: "5rem",
+                width: "7rem",
+                fontSize: "medium",
+              }}
+              type="submit"
+            >
+              Add resume
+            </button>
+            <br></br>
+            {mainResume ? (
+              <>
+              <p style={{color : '#198754'}}> Your resume has been uploaded !</p>
+                <a style={{
+                  color: "#3b32b3",
+                  border: "1px solid #3b32b3",
+                  padding: "0.3rem",
+                  borderRadius: "5rem",
+                  width: "7rem",
+                  fontSize: "medium",
+                  textDecoration : 'none'
+                }} href={mainResume} download>
+                  Download PDF
+                </a>
+              </>
+            ) : (
+              <>
+                <p style={{color : '#B54141'}}> No reume has been uploaded yet !</p>
+              </>
+            )}
+          </form>
+        </div>
+        {mainResume === "#" || mainResume === null? (
+          <></>
+        ) : (
+          <>
+            <div
+              style={{
+                maxWidth : '100%',
+                width : '100%',
+                margin: "0",
+                // padding: "2rem",
+                border: mainResume ? "1px solid grey" : "none",
+              }}
+            >
+              {console.log(mainResume)}
+              <Document
+                style={{
+                  margin: "0",
+                  padding: "0",
+                }}
+                file={mainResume}
+                onLoadSuccess={onDocumentLoadSuccess}
+                noData
+                options={{ disableAutoFetch: true }}
+              >
+                <Page
+                  style={{
+                    margin: "0",
+                    padding: "0",
+                  }}
+                  pageNumber={1}
+                  width={500}
+                  height={500}
+                  scale={1}
+                  renderTextLayer={false}
+                  renderAnnotationLayer={false}
+                />
+              </Document>
+            </div>
+          </>
+        )}
       </div>
+      </>)}
     </div>
   );
 }
