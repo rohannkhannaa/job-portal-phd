8ce10b759886cdb62f24f396b5bbecc433b92f33
Rohan Khanna <rohankhanna190@gmail.com>
2023-05-15 00:05:18 +0530

    check ok2

--- client/src/components/Academic.js
+++ client/src/components/Academic.js
@@ -381,7 +381,7 @@ export default function Profile({ user, type }) {
               </>
             ) : (
               <>
-                <div style={{ width: "80%" }} className="flex my-10 mx-20">
+                <div style={{ width: "90%" }} className="flex my-10 mx-20">
                   <div className="my-2 flex-1 bg-white shadow overflow-hidden sm:rounded-lg">
                     <div className="flex space-x-3 px-4 py-5 sm:px-6">
                       <h3 className="text-lg leading-6 font-medium text-gray-900">
